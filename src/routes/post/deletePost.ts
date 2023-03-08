723ea30170f020b063f3421527e635e144db06e2
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-03-08 11:31:57 +0100

    deletePost route added

---
+++ src/routes/post/deletePost.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response, Router } from 'express';
+import Post from 'src/models/post.model';
+
+const router = Router();
+
+router.delete('/api/post/:id', async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+
+  if(!id){
+    const error = new Error('post id is required') as CustomError;
+    error.status = 400;
+    return next(error);
+  };
+
+  try {
+    await Post.findOneAndRemove({_id: id})
+  } catch (err) {
+    next(new Error('Error occured when deleting the post'))
+  };
+
+  res.status(200).json({success: true});
+})
+
+export { router as deletePostRouter }
