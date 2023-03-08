7c85626aca671db4bd6098e9083f112d522e1d72
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-03-08 10:21:54 +0100

    initial error handling

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,27 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import express from 'express';
+import express, {NextFunction, Request, Response} from 'express';
 import mongoose from 'mongoose';
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+declare global {
+  interface CustomError extends Error {
+    status?: number
+  }
+};
+
+app.use((error: CustomError, req: Request, res: Response, next: NextFunction) => {
+  if (error.status) {
+    return res.status(error.status).json({message: error.message})
+  };
+
+  res.status(500).json({message: 'Something went wrong'})
+})
 
 const PORT = process.env.PORT || 8080;
 
