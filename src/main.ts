a5e2cc37bdf512fe1c7000bfb4610108a187821c
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-03-08 08:03:31 +0100

    mongoDB connection with mongoose

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
+import mongoose from 'mongoose'
 
 const app = express();
 
-const PORT = 8080;
-app.listen(PORT, () => {
-  console.log(`Server is listening on prt ${PORT}`);
-  
-})
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const PORT = process.env.PORT || 8080;
+
+const start = async () => {
+  if (!process.env.MONGO_URL) {
+    throw new Error('MONGO_URL is require')
+  }
+  try {
+    await mongoose.connect(process.env.MONGO_URL)
+  } catch (error) {
+    throw new Error('database connection error')
+  }
+
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
+};
+
+start();
