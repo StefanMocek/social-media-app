c5f912fa4fb9641e25e90615db594a6f8e3a0599
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-03-08 13:09:26 +0100

    get all posts, get post by Id routes

---
+++ src/routes/post/getPost.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response, Router } from 'express';
+import Post from 'src/models/post.model';
+
+const router = Router();
+
+router.get('/api/post/:id', async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+
+  if(!id){
+    const allPosts = await Post.find();
+    return res.status(200).json(allPosts);
+  };
+
+  let post;
+
+  try {
+    post = await Post.findOne({_id: id})
+  } catch (err) {
+    const error = new Error('cannot find the post') as CustomError;
+    error.status = 400;
+    return next(error);
+  };
+
+  res.status(200).json(post);
+})
+
+export { router as getPostRouter }
