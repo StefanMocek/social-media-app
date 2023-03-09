2afd2095a3f2ed6255f1426302361f1b427f9880
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-03-09 19:58:04 +0100

    error handling middleware applied

--- common/index.ts
+++ common/index.ts
@@ -2,6 +2,7 @@ export * from './src/services/authentication';
 
 export * from './src/middlewares/currentUser';
 export * from './src/middlewares/requireAuth';
+export * from './src/middlewares/error-handler';
 
 export * from './src/errors/bad-request-error';
 export * from './src/errors/custom-error';
--- common/src/middlewares/currentUser.ts
+++ common/src/middlewares/currentUser.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response, Router } from 'express';
+import { NextFunction, Request, Response} from 'express';
 import jwt from 'jsonwebtoken';
 
 declare global {
