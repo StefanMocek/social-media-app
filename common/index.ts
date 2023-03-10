70d03ee0b664e0c9ac3844fc55c6bd608849d5c1
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-03-10 17:12:46 +0100

    upload images middleware added

--- common/index.ts
+++ common/index.ts
@@ -3,6 +3,7 @@ export * from './src/services/authentication';
 export * from './src/middlewares/currentUser';
 export * from './src/middlewares/requireAuth';
 export * from './src/middlewares/error-handler';
+export * from './src/middlewares/upload-images';
 
 export * from './src/errors/bad-request-error';
 export * from './src/errors/custom-error';
---
+++ common/src/middlewares/upload-images.ts
@@ -0,0 +1,14 @@
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null, '/upload')
+  },
+  filename(req, file, callback) {
+    callback(null, file.fieldname + '-' + Date.now())    
+  },
+});
+
+const upload = multer({storage}).array('image');
+
+export {upload as uploadImages}
