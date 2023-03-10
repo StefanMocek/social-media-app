d8ffabef1f4ec9c4dccda6d671d9b971f922e1eb
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-03-10 17:39:17 +0100

    images mid applied to newPost route

--- src/routes/post/newPost.ts
+++ src/routes/post/newPost.ts
@@ -1,20 +1,39 @@
+import fs from 'fs';
+import path from 'path';
 import { NextFunction, Request, Response, Router } from 'express';
 import Post from '../../models/post.model';
 import User from '../../models/user.model';
-import { BadRequestError } from '../../../common';
+import { BadRequestError, uploadImages} from '../../../common';
 
 const router = Router();
 
-router.post('/api/post', async (req: Request, res: Response, next: NextFunction) => {
+router.post('/api/post',uploadImages , async (req: Request, res: Response, next: NextFunction) => {
   const {title, content} = req.body;
 
+  if(!req.files) {
+    return next(new BadRequestError('images are required'));
+  };
+
+  let images: Array<Express.Multer.File>;
+
+  if(typeof req.files === 'object') {
+    images = Object.values(req.files)
+  } else {
+    images = req.files ? [...req.files] : []
+  }
+
   if(!title || !content){
-    return next(new BadRequestError('title and content are required')) ;
+    return next(new BadRequestError('title and content are required'));
   };
 
   const newPost = Post.build({
     title, 
-    content
+    content,
+    images : images.map((file: Express.Multer.File) => {
+      let srcObj = { src : `data:${file.mimetype};base64,${fs.readFileSync(path.join('upload/' + file.filename)).toString('base64')}`};
+      fs.unlink(path.join('upload/' + file.filename), () => {})
+      return srcObj
+    })
   });
 
   await newPost.save();
