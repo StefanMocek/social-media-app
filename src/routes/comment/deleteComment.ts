8252db06697eeb4b42bf1f5bcea5d888cf39d53d
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-03-10 15:53:01 +0100

    pull comment from post when deleted

--- src/routes/comment/deleteComment.ts
+++ src/routes/comment/deleteComment.ts
@@ -12,17 +12,20 @@ router.delete('/api/comment/:commentId/:postId', async (req: Request, res: Respo
     return next(new BadRequestError('post and comment ids are required')); 
   };
 
+  let post;
+
   try {
     await Comment.findOneAndRemove({_id: commentId});
-    await Post.findOneAndUpdate(
+    post = await Post.findOneAndUpdate(
       {_id: commentId},
-      {$pull: {comments: commentId}}
+      {$pull: {comments: commentId}},
+      {new: true}
     )
   } catch (err) {
     next(new Error('comment occured when deleting the comment'))
   };
 
-  res.status(200).json({success: true});
+  res.status(200).json(post);
 })
 
 export { router as deleteCommentRouter }
