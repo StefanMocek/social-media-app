3c045fb22acfe13ec0d88186748ec9e11d897099
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-03-12 16:56:04 +0100

    sign up test

---
+++ src/routes/auth/__test__/signup.test.ts
@@ -0,0 +1,26 @@
+import request from 'supertest';
+import {app} from '../../../app';
+
+jest.setTimeout(50000);
+
+it('returns 201 on successfull signup', async () => {
+  return request(app)
+    .post('/signup')
+    .send({
+      email: "testemail@email.com",
+      password: "abc123"
+    })
+    .expect(201)
+});
+
+it('sets cookies after successfully signup', async () => {
+  const res = request(app)
+    .post('/signup')
+    .send({
+      email: "testemail@email.com",
+      password: "abc123"
+    })
+    .expect(201)
+
+  expect(res.get('Set-Cookie')).toBeDefined()
+})
